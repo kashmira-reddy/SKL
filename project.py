768d3854ee88af8bfbf66a0292819d15aeb0c168
livmill <livmill@umich.edu>
2021-04-11 12:33:19 -0400

    comments for questions

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
-hi
+# questions
+
+# 1. do we need 3 api keys ?
+# 2. is twitter an API or website : API
+# 3. generic starting format
+# 4. our ideas:
+    #--make sure that the APIs work or have backup ready to use like reddit
+# What data will you collect from each API/website and store in a database?  Be specific.
+# API1: We will collect different breeds of dogs and pictures of them. We will look up many breeds of dogs. 
+# API2: We will find memes related to the specific breed of dog we have found using API1. We will look it up with many breeds of dogs. 
+# Website: We will look for the tweets that mention that specific breed we found in API1. We will look it up with many breeds of dogs. 
+# What data will you be calculating from the data in the database?
+# We will be calculating the number of different breeds from the dog API.
+# We will be calculating how many of these are used in memes from the meme API.
+# We will also calculate how prevalent dogs, memes, and dog memes are on Twitter.
